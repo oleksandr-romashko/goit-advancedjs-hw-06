852650a76df376b082c55d93154351a5e3f14a33
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-17 23:37:03 +0100

    Add baseTypes task-2 solution

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,27 @@
-let anything = -20;
+// Який тип ви надаєте змінній anything в TypeScript, щоб зберегти її гнучкість?
+
+// Варіант 1:
+// "unknown type is the type-safe counterpart of the any type"
+
+let anything: unknown = -20;
 anything = 'Text';
 anything = {};
 
+// Варіант 2:
+// (якщо не прив'язуватись до назви змінної 
+//  та ми знаємо що будуть лише ці два конкретні типи
+//  i ніякі інші, якщо це має сенс...)
+
+let alternativeAnything: (number | {}) = -20;
+alternativeAnything = 'Text';
+alternativeAnything = {};
+
 export {};
+
+// Варіант 3:
+// Мені здається це найгіршим варіантом, 
+// так як фактично вимикає строгу типізацію.
+
+let anyAnything: any = -20;
+anyAnything = 'Text';
+anyAnything = {};
