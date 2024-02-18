ada3ff939dff37f7704010df242bac392f2fdbc7
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-18 17:35:54 +0100

    Add generic task-2 solution

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,13 +11,16 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
+function compare <
+  T extends Pick<AllType, 'name' | 'color'>, 
+  U extends Pick<AllType, 'position' | 'weight'>
+  > (top: T, bottom: U): AllType {
+    return {
+      name: top.name,
+      color: top.color,
+      position: bottom.position,
+      weight: bottom.weight,
+    }
 }
 
 export {};
