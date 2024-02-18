bc3a86a9ed84c7ad59d0169d384955ce09276b14
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-18 12:45:41 +0100

    Add baseTypes task-3 solution

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,11 @@
+// Що потрібно виправити в цьому коді, щоб він став правильним та безпечним?
+
 let some:unknown;
 some = 'Text';
 let str: string;
-str = some;
+
+if (typeof some === "string") {   // typeof type guard
+  str = some;
+}
 
 export {};
